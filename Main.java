d498eb1440ca46765c9ad8b43248a06ab2b32ba9
younj1601 <younj1601@wrdsb.ca>
2020-09-15 15:30:45 +0000

    Finished question 3 for conditions

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program will put to numbers in the right order
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    //prompt for first integer
+    System.out.println("Please enter an integer:");
+    //get integer from user
+    int num1 = input.nextInt();
+
+    //prompt for second integer
+    System.out.println("Please enter another integer:");
+    //get next integer from user
+    int num2 = input.nextInt();
+
+    if (num1 <= num2){
+      // numbers are the same so order doesn,t matter
+      //num1 goes first because smaller
+      System.out.println("Your numbers in ascending order are " + num1 + ", " + num2);
+    } else {
+      //num2 is smaller is small so it goes first
+      System.out.println("Your numbers in ascending order are " + num2 + ", " + num1);
+    }
+  
   }
 }
